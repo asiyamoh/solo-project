68d9837c25381f784763d67e22fcfba6252d5776
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-09-05 19:50:29 -0500

    working on the client side POST

--- src/redux/sagas/addBoxer.saga.js
+++ src/redux/sagas/addBoxer.saga.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
 import { put, takeLatest } from 'redux-saga/effects';
 
-function* addBoxer(){
-    yield axios.post('/api/addBoxer', )
+function* addBoxer(action) {
+    console.log('In HERE!')
+    try {
+       const response = yield axios.post('/api/addBoxer', action.payload)
+       console.log(response)
+    } catch (error) {
+        console.log('error with the POST', error)
+    }
 }
 
-function* addSaga(){
+function* addSaga() {
     yield takeLatest('ADD_BOXER', addBoxer)
 }
 
-export  default addSaga();
+export default addSaga;
