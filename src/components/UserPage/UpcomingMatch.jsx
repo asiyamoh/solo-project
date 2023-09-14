308a42440078c18b3a9e91caf1911e96cce0a4ae
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-09-14 16:28:00 -0500

    working on the happened button

--- src/components/UserPage/UpcomingMatch.jsx
+++ src/components/UserPage/UpcomingMatch.jsx
@@ -1,15 +1,38 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 
-function UpcomingMatch(){
-    const upcoming = useSelector((store) => store.getUpcomingMatch)
-    console.log('the store upcoming:', upcoming)
+function UpcomingMatch() {
+  const upcomings = useSelector((store) => store.getUpcomingMatch);
+  console.log("the store upcoming:", upcomings);
 
-    return(
-        <>
-        <h1>Upcoming Match</h1>
-        
-        </>
-    )
+  const handleHappened = () => {
+    console.log('Happened!');
+    
+  }
+
+  return (
+    <>
+      <h1>Upcoming Match</h1>
+      <div>
+        {upcomings.map((upcoming) => {
+          return (
+            <div>
+              <h3>
+                {upcoming.member1_firstname} {upcoming.member1_lastname}
+                {" VS "}
+                {upcoming.member2_firstname} {upcoming.member2_lastname}
+              </h3>
+              <div>
+                <h3>Location: {upcoming.location}</h3>
+                <h3>weigh in time: {upcoming.weigh_time}</h3>
+                <h3>Door opens: {upcoming.show_time}</h3>
+              </div>
+              <button onClick={handleHappened}>Happened</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default UpcomingMatch
+export default UpcomingMatch;
