d36dfbaa4bcbb7177427143060e036670db63706
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-09-13 19:34:47 -0500

    got the incoming request on the home page

--- src/components/UserPage/IncomingRequestInfo.jsx
+++ src/components/UserPage/IncomingRequestInfo.jsx
@@ -1,14 +1,21 @@
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 
 function IncomingRequestInfo() {
-
-    const incomingRequest = useSelector((store) => store.incomingRequests)
-    console.log('almost there:', incomingRequest);
-
+  const incomingRequest = useSelector((store) => store.incomingRequests);
+  console.log("almost there:", incomingRequest);
 
   return (
     <>
       <h1>Incoming Requests</h1>
+
+      {incomingRequest.map((incoming) => {
+        return (
+          <>
+            <div>{incoming.first_name}</div>
+          </>
+        );
+      })}
+      
     </>
   );
 }
