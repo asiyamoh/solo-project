696e1e0aefb029ec47446b46684e01125a3cf80b
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-09-18 12:48:23 -0500

    fixed the load issue with the  result page

--- src/components/RequestTheMatch/RequestTheMatch.jsx
+++ src/components/RequestTheMatch/RequestTheMatch.jsx
@@ -39,14 +39,14 @@ function RequestTheMatch() {
     return (
         <>
             <h1>Request the match</h1>
-            <h3>
+            {/* <h3> */}
                 {/* return specific detalis of the boxer */}
-                {specificBoxer[0].firstname}
-                {specificBoxer[0].lastname}
-                {specificBoxer[0].weight_class}
-                {specificBoxer[0].coach_name}
-                {specificBoxer[0].fights_count}
-            </h3>
+                {/* {specificBoxer[0].firstname}
+                {specificBoxer[0].lastname} */}
+                {/* {specificBoxer[0].weight_class}
+                {specificBoxer[0].coach_name} */}
+                {/* {specificBoxer[0].fights_count} */}
+            {/* </h3> */}
 
             <div>
                 <FightDates />
