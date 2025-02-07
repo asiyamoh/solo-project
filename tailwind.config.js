44574c88837b0a4e1e7d58959d958d3bdd7c7a4e
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2025-02-07 09:16:25 -0600

    added some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  content:  [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
